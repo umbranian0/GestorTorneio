613716217ff8deb3645924d934512894e0d79b4b
João Lopes <35259740+joaodlopes@users.noreply.github.com>
2019-06-13 15:38:06 +0100

    ...

--- index2.js
+++ index2.js
@@ -1,64 +1,73 @@
-var gamesArray = {
-  equipas: [
-    ["Team 1"], ["Team 2"], /* first matchup */
-    ["Team 3"], ["Team 4"]
-  ]  /* second matchup */,
-  espacoResultados:             // List of brackets (single elimination, so only one bracket)
-    [                     // List of rounds in bracket
-      [                   // First round in this bracket
-        [1, 2],           // Team 1 vs Team 2
-        [3, 4]            // Team 3 vs Team 4
-      ],
-      [                   // Second (final) round in single elimination bracket
-        [5, 6],           // Match for first place
-        [7, 8]            // Match for 3rd place
-      ]
-    ]
-};
+window.onload = boot;
 
-var knownBrackets = [2, 4, 8, 16, 32];// brackets with "perfect" proportions (full fields, no byes)
+var ID_SECTION_FEEDBACK = "idSectionFeedback";
 
-var exampleTeams;
-var bracketCount;
+var gamesArray = { equipas: [] };
+
+
+var oSectionFeedback, listOrdered, bracketCount;
 
 //organized games array
-var listOrdered ;
+var arrayEquipas = [];
 
 window.addEventListener("load", boot, false);
 
 function boot() {
-  bracketCount = 0;
-  
-  gamesArray.equipas = lerK("key_arrayEquipas");
+  oSectionFeedback = $(ID_SECTION_FEEDBACK);
 
-  console.log(gamesArray.equipas);
-  
+  bracketCount = 0;
+
+  gamesArray.equipas =  criaArrayEquipas();
+
   listOrdered = shuffle(gamesArray.equipas);
 
+  console.log(arrayEquipas);
 
- // getBracket();
+
+  inserirEquipasnaPagina_Teste();
+
+  // getBracket();
 }//boot
 
-function shuffle(array){
+function criaArrayEquipas() {
+  console.log(lerK("key_arrayEquipas_1"));
+  for (var i = 1; i <= lerK("key_quantidade"); i++) {
+    arrayEquipas[i] = lerK("key_arrayEquipas_" + i);
+  }
+return arrayEquipas;
+
+}//criaArrayEquipas
+
+function inserirEquipasnaPagina_Teste() {
+  var strRet = arrayEquipas[0] + "<br>";
+  for (var i = 0; i <= lerK("key_quantidade"); i++) {
+
+    strRet += arrayEquipas[i + 1] + "<br>";
+
+  };
+  oSectionFeedback.innerHTML = oSectionFeedback.innerHTML + strRet;
+
+  return false;
+
+}//inserirEquipasnaPagina_Teste
+
+function shuffle(array) {
   //variaveis de auxilio
   let j, x, i;
   //ciclo que percorre o array para organizar o codigo
   for (i = array.length - 1; i > 0; i--) {
-      j = Math.floor(Math.random() * (i + 1));
-      x = array[i];
-      array[i] = array[j];
-      array[j] = x;
+    j = Math.floor(Math.random() * (i + 1));
+    x = array[i];
+    array[i] = array[j];
+    array[j] = x;
   }//ciclo
- //console.log(array);
+  //console.log(array);
   return array;
- 
+
 }//suffle
 
 function getBracket(base) {
   
-
-
-
   renderBrackets(brackets);
 }//get brackets
 
