3993eef7e44573cf386b9b477b91b60d7ecf1839
João Lopes <35259740+joaodlopes@users.noreply.github.com>
2019-06-15 14:35:44 +0100

    Final Version

--- index2.js
+++ index2.js
@@ -1,41 +1,73 @@
 window.onload = boot;
 
-var ID_SECTION_FEEDBACK = "idSectionFeedback";
+var ID_SECTION_FEEDBACK = "idSectionFeedback",
+  ID_SUBMIT_GERAR_NOVA = "idSubmitGerarNova";
 
-var oSectionFeedback, listOrdered, bracketCount;
+
+var oSectionFeedback, oSubmitGerarNova, strRet;
 
 //organized games array
 var arrayEquipas = [];
 
+
 function boot() {
   oSectionFeedback = $(ID_SECTION_FEEDBACK);
-
-  bracketCount = 0;
+  oSubmitGerarNova = $(ID_SUBMIT_GERAR_NOVA);
 
   criaArrayEquipas();
 
-  // getBracket();
+  oSubmitGerarNova.onclick = removerEquipasDerrotadas;
+
 }//boot
 
 function criaArrayEquipas() {
+  console.log("Entrei");
   for (var i = 0; i < lerK("key_quantidade"); i++) {
-    arrayEquipas[i] = lerK("key_arrayEquipas_" + (i + 1));
+    arrayEquipas[i] = lerK("key_arrayEquipas_" + (i));
   }
+  shuffle(arrayEquipas);
 
-  inserirEquipasnaPagina_Teste();
 
 }//criaArrayEquipas
 
-function inserirEquipasnaPagina_Teste() {
-  var strRet = "<h2>" + lerK("key_nome") + "</h2>" + "<hr>";
-  for (var i = 0; i < lerK("key_quantidade"); i++) {
-    strRet += arrayEquipas[i] + "<hr>";
+function inserirEquipasnaPagina(array) {
+  strRet = "<fieldset><legend> Torneio: " + lerK("key_nome") + "</legend>";
+  for (var i = 0; i < lerK("key_quantidade") / 2; (i++)) {
+    if (array[i] === array[lerK("key_quantidade") - 1]) {
+      strRet += "<fieldset><legend><mark> Final do Jogo </mark></legend>" + array[i] + " venceu o Torneio</fieldset>" + "<hr>";
+      oSubmitGerarNova.hidden = true;
+    }
+    else if (array[i] !== array[lerK("key_quantidade") - 1 - i]) {
+      strRet += "<fieldset><legend><mark> Jogo " + (i + 1) + "</mark></legend><input type='radio' name='" + i + "' id='idVencedor_" + i + "'>  " + array[i] + " vs. "
+        + array[lerK("key_quantidade") - 1 - i] + "  <input type='radio' name='" + i + "' id='idVencedor_" + (lerK("key_quantidade") - 1 - i) + "'></fieldset>" + "<hr>";
+    }
+    else {
+      strRet += "<fieldset><legend><mark> Jogo " + (i + 1) + "</mark></legend><input type='radio' checked='checked' hidden name='" + i + "' id='idVencedor_" + i + "'>" + array[i] + " passa à proxima ronda</fieldset>" + "<hr>";
+    }
   };
+  strRet += "</fieldset>";
   oSectionFeedback.innerHTML = strRet;
 
   return false;
 
 }//inserirEquipasnaPagina_Teste
+
+function removerEquipasDerrotadas() {
+  let arrayAtualizado = [];
+  for (var i = 0, j = 0; i < lerK("key_quantidade"); i++) {
+    if (document.getElementById("idVencedor_" + i).checked) {
+      arrayAtualizado[j] = arrayEquipas[i];
+      j++
+    } else {
+      console.log("Removeu " + lerK("key_arrayEquipas_" + (i)));
+      removerK(lerK("key_arrayEquipas_" + (i)));
+    }
+  }
+  escreverKV("key_quantidade", arrayAtualizado.length);
+  arrayEquipas = arrayAtualizado;
+  shuffle(arrayEquipas);
+
+}//removerEquipasDerrotadas
 
 function shuffle(array) {
   //variaveis de auxilio
@@ -47,20 +79,7 @@ function shuffle(array) {
     array[i] = array[j];
     array[j] = x;
   }//ciclo
-  //console.log(array);
-  return array;
+  inserirEquipasnaPagina(array);
+  arrayEquipas = array;
 
-}//suffle
-
-function getBracket(base) {
-
-  renderBrackets(brackets);
-}//get brackets
-
-/*
- * Inject our brackets
- */
-
-function guardaDados() {
-
-}
+}//shuffle
